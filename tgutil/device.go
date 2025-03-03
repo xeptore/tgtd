6e0ceae13ef116b8e7f10cf3785cb845f57b48bc
xeptore <29199390+xeptore@users.noreply.github.com>
2025-03-03 10:23:17 +0330

    lint: fix error
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

--- tgutil/device.go
+++ tgutil/device.go
@@ -2,16 +2,13 @@ package tgutil
 
 import (
 	"github.com/gotd/td/telegram"
-	"github.com/gotd/td/tg"
 )
 
-var Device = telegram.DeviceConfig{
+var Device = telegram.DeviceConfig{ //nolint:exhaustruct
 	DeviceModel:    "Desktop",
 	SystemVersion:  "Windows 10",
 	AppVersion:     "4.2.4 x64",
 	LangCode:       "en",
 	SystemLangCode: "en-US",
 	LangPack:       "tdesktop",
-	Proxy:          tg.InputClientProxy{},
-	Params:         nil,
 }
