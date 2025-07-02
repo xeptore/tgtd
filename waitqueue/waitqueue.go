36e24200b3268de6cc79e919470ed304fb50c116
xeptore <29199390+xeptore@users.noreply.github.com>
2025-07-02 15:32:38 +0330

    fix: fix wait-queue infinite loop
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

--- waitqueue/waitqueue.go
+++ waitqueue/waitqueue.go
@@ -68,6 +68,7 @@ func (w *WaitQueue) SendMany(ctx context.Context, n int32, fn func() error) erro
 				}
 				return err
 			}
+			return nil
 		}
 	}
 }
@@ -83,6 +84,7 @@ func (w *WaitQueue) trySend(fn func() error, n int32) error {
 			return err
 		}
 		w.intervalCounter.Add(n)
+		return nil
 	}
 	return errIntervalCapReached
 }
